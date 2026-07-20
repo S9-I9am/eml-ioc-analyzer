2c2ab19f106cdbfa32c2bb9a1763954221ac4deb
S9-I9am <staphy2006@gmail.com>
2026-07-20 11:46:11 +0100

    Add IOC extractor for URL detection

---
+++ ioc/extractor.py
@@ -0,0 +1,12 @@
+import re
+class IOCExtractor:
+    def __init__(self, email_data):
+       self.email_data = email_data
+    def extract_urls(self):
+        urls = []
+        body = self.email_data.get("body")
+        if body:
+            pattern = r"https://[^\s]+"
+            urls = re.findall(pattern, body)
+        return urls    
+            
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from parser.eml_parser import EMLParser
+from ioc.extractor import IOCExtractor
 
 parser = EMLParser("test_attachment.eml")
 
 resultat = parser.parse()
 
+extractor = IOCExtractor(resultat)
+
+urls = extractor.extract_urls()
+
 print(resultat)
+print(urls)
